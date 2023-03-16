ebcbdb5d3783f7c2891c763633bbfc0d84f56c87
Suhitha bala <92919883+ssuhitha@users.noreply.github.com>
2023-03-16 15:57:28 +0530

    Add files via upload
    Java Lab 160323

---
+++ Records/21CYS/CB.EN.U4CYS21033/160323_cys21033/addition/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Addition of two numbers");
+        int j = 4;
+        int k =3;
+        int l=j+k;
+        System.out.println(l);
+    }
+}
